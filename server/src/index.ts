4ea459776a5dd6a538d2f77d08925b08f3a99a7f
Brandon Minjares <brandon.minjares@gmail.com>
2024-07-21 17:40:24 -0700

    removed useless code

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,11 @@ import fs from 'fs';
 import csv from 'csv-parser';
 import cors from 'cors';
 const { stringify } = require('csv-stringify/sync');
+const csvFilePath = './data/data.csv'
 
 interface CsvRow {
   ID: string,
   Info: string
-  // Add more fields as per your CSV structure
 }
 
 const app = express();
@@ -15,28 +15,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 const port = 3001;
 
 app.get('/data', (req: Request, res: Response) => {
   const results: CsvRow[] = [];
 
-  fs.createReadStream('./data/data.csv')
+  fs.createReadStream(csvFilePath)
   .pipe(csv())
   .on('data', (data: CsvRow) => results.push(data))
   .on('end', () => {
     res.send(results);
-    // Now `results` is typed as an array of CsvRow objects
   });
 });
 
 app.post('/data', (req: Request, res: Response) => {
-
   const csvData = req.body.map((row: any) => Object.values(row).join(',')).join('\n');
   const csvHeaders = Object.keys(req.body[0]).join(',');
 
-  fs.writeFile('./data/data.csv', `${csvHeaders}\n${csvData}`, (err) => {
+  fs.writeFile(csvFilePath, `${csvHeaders}\n${csvData}`, (err) => {
     if (err) {
       return res.status(500).send('Error writing to CSV file');
     }
@@ -44,13 +40,8 @@ app.post('/data', (req: Request, res: Response) => {
   });
 });
 
-app.delete('/data/:rowID', (req: Request, res: Response) => {
-  console.log('tes')
-  console.log('row is ' + req.params['rowID'])
-  
+app.delete('/data/:rowID', (req: Request, res: Response) => {  
   let results: CsvRow[] = [];
-
-  const csvFilePath = './data/data.csv'
 
   fs.createReadStream(csvFilePath)
     .pipe(csv())
@@ -78,21 +69,8 @@ app.delete('/data/:rowID', (req: Request, res: Response) => {
             console.log('Row deleted and CSV file updated.');
         });
     });
-    
-/*
-  const csvData = req.body.map((row: any) => Object.values(row).join(',')).join('\n');
-  const csvHeaders = Object.keys(req.body[0]).join(',');
-
-  fs.writeFile('./data/data.csv', `${csvHeaders}\n${csvData}`, (err) => {
-    if (err) {
-      return res.status(500).send('Error writing to CSV file');
-    }
-    res.send('CSV file updated successfully');
-  });
-  */
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
